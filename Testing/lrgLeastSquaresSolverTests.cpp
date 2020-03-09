ac439a4f7b27b8c3591382674068cebd7fd355bb
sukrire <sandromarcus@hotmail.co.uk>
2020-03-09 11:57:08 +0000

    missing a lose bracket

--- Testing/lrgLeastSquaresSolverTests.cpp
+++ Testing/lrgLeastSquaresSolverTests.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 TEST_CASE( "Simple add", "[MyFirstAddFunction]") {
-  REQUIRE( lrg::LinearDatacreator() != 0;
+  REQUIRE( lrg::LinearDatacreator() != 0;)
 }
 
 // somehow need to get it run this class, might be easier to do once it has a function?
