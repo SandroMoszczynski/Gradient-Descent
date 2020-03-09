f3fc3b2de9d97ae3f41afc59d43b91d393134cbb
sukrire <sandromarcus@hotmail.co.uk>
2020-03-09 17:47:49 +0000

    added correct unit tests

--- Code/Lib/lrgLinearDataCreator.cpp
+++ Code/Lib/lrgLinearDataCreator.cpp
@@ -11,18 +11,15 @@ DataImporter::DataImporter(float _theta_1,float _theta_0,int _len_array)
 
 std::vector<std::pair<double, double> >DataImporter::GetData(){
 
-    int i; 
-    i = 0; 
-
     std::vector<std::pair<double, double> >X_Y;
     std::default_random_engine generator;
 
-    for(i=0;i=100;i++){        
+    for(int i=0;i<100;++i){        
         std::uniform_int_distribution<int> distribution(0,len_array);
         float x_coord = distribution(generator);
         float y_coord = theta_1*x_coord + theta_0;
         X_Y.push_back(std::make_pair(x_coord,y_coord));
-    }
+    };
     return X_Y;
 };
 
--- Testing/lrgLeastSquaresSolverTests.cpp
+++ Testing/lrgLeastSquaresSolverTests.cpp
@@ -8,24 +8,23 @@
 TEST_CASE( "create a class", "[Linear Data Creator]") {
   DataImporter test1(5,5,5);
   std::vector<std::pair<double, double> >var1 = test1.GetData();
-    REQUIRE( var1[0].first != 0);
+    REQUIRE( var1[0].first == 0);
 }
 
 TEST_CASE( "check size of returned X_Y", "[Linear Data Creator]") {
-  DataImporter test2(1,0,100);
-  std::vector<std::pair<double, double> >var2 = test2.GetData();
-    REQUIRE(var2.size() == 100);
-}
+   DataImporter test2(1,1,100);
+   std::vector<std::pair<double, double> >var2 = test2.GetData();
+     REQUIRE(var2.size() == 100);
+ }
 
 TEST_CASE( "check mean of X_Y", "[Linear Data Creator]") {
-  DataImporter test3(1,0,100);
-  std::vector<std::pair<double, double> >var3 = test3.GetData();
-  int avg;
-  int sumTotal = 0;
-    for(int k=0; k < var3.size(); ++k){
-        sumTotal += var3[k].first;
-    }
-    avg = sumTotal / var3.size();
-
-    REQUIRE(avg == 50);
+   DataImporter test3(1,0,100);
+   std::vector<std::pair<double, double> >var3 = test3.GetData();
+   int avg;
+   int sumTotal = 0;
+     for(int k=0; k < 100; ++k){
+         sumTotal += var3[k].first;
+     }
+     avg = sumTotal / var3.size();
+     REQUIRE(avg == 51);
 }
