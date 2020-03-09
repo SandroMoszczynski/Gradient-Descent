b9b4ff51103d9d61107078523a9ac0a1d6c42af5
sukrire <sandromarcus@hotmail.co.uk>
2020-03-09 17:02:31 +0000

    added most of question 2 and q1

--- Code/Lib/lrgLinearDataCreator.cpp
+++ Code/Lib/lrgLinearDataCreator.cpp
@@ -1,31 +1,30 @@
 #include "lrgLinearDataCreator.h"
 #include <vector>
+#include <random>
 
-// create a for loop where i read each part of a text file and append it to a list
-
-int main(){
-    double *Y, *Data;
-    int noise;
-    int theta_0;
-    int theta_1 ;
-    int i;
-    
-    std::vector<int> X(100);   // defined a vector of size 100
-
-
-    theta_0 = 5;
-    theta_1 = 5;
-    noise = 5;
-
-
-    for(i=1;i=100;i++){
-        Data[i]= theta_1*X[i] + theta_0 + noise;
-    }
-
-    Y = Data;
-
-    return 0;
+DataImporter::DataImporter(float _theta_1,float _theta_0,int _len_array)
+{
+    theta_1 = _theta_1;
+    theta_0 = _theta_0; 
+    len_array = _len_array;
 }
+
+std::vector<std::pair<double, double> >DataImporter::GetData(){
+
+    int i; 
+    i = 0; 
+
+    std::vector<std::pair<double, double> >X_Y;
+    std::default_random_engine generator;
+
+    for(i=0;i=100;i++){        
+        std::uniform_int_distribution<int> distribution(0,len_array);
+        float x_coord = distribution(generator);
+        float y_coord = theta_1*x_coord + theta_0;
+        X_Y.push_back(std::make_pair(x_coord,y_coord));
+    }
+    return X_Y;
+};
 
 
 
