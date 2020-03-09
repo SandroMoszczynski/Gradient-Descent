3bbc4bb237b6f4883de6b9c0dac424bf1d8859b7
sukrire <sandromarcus@hotmail.co.uk>
2020-03-09 12:56:05 +0000

    added a way to generate y=mx+c+noise

--- Code/Lib/lrgLinearDataCreator.cpp
+++ Code/Lib/lrgLinearDataCreator.cpp
@@ -1,9 +1,29 @@
 #include "lrgLinearDataCreator.h"
+#include <vector>
 
 // create a for loop where i read each part of a text file and append it to a list
 
 int main(){
-    double *X,*Y ;
+    double *Y, *Data;
+    int noise;
+    int theta_0;
+    int theta_1 ;
+    int i;
+    
+    std::vector<int> X(100);   // defined a vector of size 100
+
+
+    theta_0 = 5;
+    theta_1 = 5;
+    noise = 5;
+
+
+    for(i=1;i=100;i++){
+        Data[i]= theta_1*X[i] + theta_0 + noise;
+    }
+
+    Y = Data;
+
     return 0;
 }
 
