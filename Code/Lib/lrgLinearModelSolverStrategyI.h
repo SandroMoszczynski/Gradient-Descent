a5725dd35c7eab93ec03497b46f7965e3b04a92e
sukrire <sandromarcus@hotmail.co.uk>
2020-03-10 10:38:53 +0000

    added files for this question and populated them as asked

---
+++ Code/Lib/lrgLinearModelSolverStrategyI.h
@@ -0,0 +1,7 @@
+#include <memory>
+#include <vector>
+
+class DataImporterI {
+    public:
+    virtual std::pair<double, double> FitData(std::vector<std::pair<double, double> >)
+};
---
+++ Code/Lib/lrgNormalEquationSolverStrategy.h
@@ -0,0 +1,7 @@
+#include "lrgDataCreatorI.h"
+
+class DataImporter : public DataImporterI {
+    public:
+    // DataImporter(float theta_1,float theta_0, int len_array);
+    std::pair<double, double> FitData(std::vector<std::pair<double, double> >)
+};
